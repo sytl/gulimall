bcf9e9cd1fe0d9450f34c3150f8bb0785e661c4b
Felix <1594650290@qq.com>
2021-03-06 17:05:28 +0800

    修改2

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -2,8 +2,12 @@ package com.atguigu.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+//@EnableConfigurationProperties
 public class GulimallCouponApplication {
 
     public static void main(String[] args) {
